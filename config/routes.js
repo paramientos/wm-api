9d7556d94e864f76fe82a007b223cb95dcab74df
sosi <soysal.tan@emlak24.com>
2021-11-08 15:48:39 +0300

    Add ApiDoc library and generate one

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,28 @@
 
 module.exports.routes = {
 
+  /**
+   * @api {get} /partners/distances?distance&unit&order Get nearest partner to given distance
+   * @apiName GetPartner Distance
+   * @apiGroup Partner
+   *
+   * @apiParam {Number} distance default is 0
+   * @apiParam {String} [unit] Optional.Default value is 'K' which means KM (can be 'K' or 'N')
+   * @apiParam {Number} [order] Optional.Default value is 'a' which means ascending order (can be 'a' or 'd')
+   *
+   * @apiSuccess {PartnerItem} Partner location Json information
+   */
   'GET /partners/distances': 'PartnerController.getAllByDistance',
+
+  /**
+   * @api {get} /partners/:id Get partner information
+   * @apiName GetPartner
+   * @apiGroup Partner
+   *
+   * @apiParam {Number} id Partner unique ID.
+   *
+   * @apiSuccess {Object} Partner Json information
+   */
   'GET /partners/:id': 'PartnerController.get',
 
 };
